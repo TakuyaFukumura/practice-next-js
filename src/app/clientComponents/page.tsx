f0f60fd96c9b193af073936029d67e89c7da9695
takuya-fukumura <blackchoco1114@gmail.com>
2025-03-20 23:33:19 +0900

    クリックされた時の処理呼び出しを追加

--- src/app/clientComponents/page.tsx
+++ src/app/clientComponents/page.tsx
@@ -21,10 +21,23 @@ export default function Call() {
     fetchData();
   }, []);
 
+  const handleClick = async () => {
+      try {
+        const apiResponse = await fetch("/api/click"); // APIエンドポイント
+        if (!apiResponse.ok) throw new Error("Failed to fetch data");
+        const data = await apiResponse.json();
+        setMessage(data.message);
+      } catch (error) {
+        console.error(error);
+        setMessage("エラーが発生しました");
+      }
+    };
+
   return (
     <main>
       <h1>APIの呼び出し結果</h1>
       <p>{message}</p>
+      <button onClick={handleClick}>クリックしてデータを取得</button>
     </main>
   );
 }
